9c84860312008bdda3dce260a827bd6e4484e7f3
Ladislav Husty <lada.husty@gmail.com>
2024-04-04 13:42:14 +0200

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
 	faPlay,
