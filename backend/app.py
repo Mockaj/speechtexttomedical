a9057d186eefa47640e3dcb39ad44606e649e9ff
Ladislav Husty <lada.husty@gmail.com>
2024-05-01 21:24:00 +0200

    added recommendations feature

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,9 @@
+import os
+import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from openai import OpenAI
-import os
-import json
-
 
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
@@ -30,7 +29,8 @@ def upload_file():
     if file.filename:
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filepath)
-        return jsonify({"message": "File uploaded successfully", "path": filepath}), 200
+        return jsonify({"message": "File uploaded successfully",
+                        "path": filepath}), 200
 
 
 @app.route('/transcribe', methods=['GET'])
@@ -48,28 +48,30 @@ def transcribe_audio():
                 model="whisper-1",
                 file=audio_file,
                 response_format="text",
-                language="cs"
+                language="cs",
+                prompt="Tobradex, Maxitrol, Lumigan, Pataday, Xalatan, Systane"
             )
 
             postprocessed_text = postprocess_text(transcription)
             return jsonify({"transcription": postprocessed_text}), 200
     except Exception as e:
+        print(str(e))
         return jsonify({"error": str(e)}), 500
 
 
 def postprocess_text(text: str) -> dict:
     completion = client.chat.completions.create(
-        model="gpt-4-turbo-preview",
+        model="gpt-4-turbo-preview",  # gpt-3.5-turbo, gpt-4-turbo-preview
         response_format={"type": "json_object"},
         temperature=0.0,
         messages=[
             {"role": "system", "content": """Jsi profesionální copywriter a tvůj úkol je opravit překlepy a gramatické chyby v následujícím textu.
-             Text se týká oftalmologie a obsahuje odborné výrazy. Ujisti se, že obzvláště tyto léky a oční kapky jsou správně napsány: 
-             Pokus se z kontextu odhadnout, co by mohlo být správné. Zde je seznam očních kapek, které se v textu často vyskytují: Tobradex, Maxitrol.
-             Pokud v textu bude zmínka o doporučeních chci aby jsi vrátil v jsonu také klíč "recommendations" a hodnotou s polem jednotlivých doporučení.
-             Vracej json ve formátu {"text": "opravený text", "recommendations": [recommendation1, recommendation2, ...], pokud v textu nebude žádná zmínka
-             o doporučeních, vracej pro klíč recommendations prázdné pole.}
-            V případě, že si nejsi jistý, můžeš nechat text nezměněný. Odpovídej POUZE V ČEŠTINĚ."""},
+             Text se týká oftalmologie a obsahuje odborné výrazy. Ujisti se, že obzvláště tyto léky a oční kapky jsou správně napsány:
+             Tobradex, Maxitrol, Lumigan, Pataday, Xalatan, Systane.
+             Pokud v textu bude zmínka o doporučeních, chci aby jsi vrátil v jsonu také klíč "recommendations" a hodnotou s polem jednotlivých doporučení.
+             Vracej json ve formátu {"text": "opravený text", "recommendations": [doporučení]. Pokud v textu nebude žádná zmínka
+             o doporučeních, vracej pro klíč recommendations prázdné pole. V případě, že se v textu zmínka o doporučení vyskytne, neuváděj
+             tuto část ve výsledné hodnotě pro klíč "text".} V případě, že si u některého slova jistý, může ho nechat nezměněné. Odpovídej POUZE V ČEŠTINĚ."""},
             {"role": "user", "content": "Zde je zmíněný text: " + text}
         ]
     )
@@ -79,7 +81,7 @@ def postprocess_text(text: str) -> dict:
         response_text = response.get("text")
     else:
         response_text = response.get(
-            "text") + "\nDoporučuji:\n" + "\n".join([f"• {rec}" for rec in response.get("recommendations", [])])
+            "text") + "\nDoporučuji:\n" + "\n".join([f"• {rec}" for rec in response.get("recommendations")])
     return response_text
 
 
