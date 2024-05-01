61bf33fb84ec278c38087001c09f4861f1830fe3
Ladislav Husty <lada.husty@gmail.com>
2024-05-01 15:33:02 +0200

    Added gpt4 post-processing

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 from openai import OpenAI
 import os
+import json
 
 
 load_dotenv()
@@ -49,9 +50,37 @@ def transcribe_audio():
                 response_format="text",
                 language="cs"
             )
-            return jsonify({"transcription": transcription}), 200
+
+            postprocessed_text = postprocess_text(transcription)
+            return jsonify({"transcription": postprocessed_text}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+def postprocess_text(text: str) -> dict:
+    completion = client.chat.completions.create(
+        model="gpt-4-turbo-preview",
+        response_format={"type": "json_object"},
+        temperature=0.0,
+        messages=[
+            {"role": "system", "content": """Jsi profesionální copywriter a tvůj úkol je opravit překlepy a gramatické chyby v následujícím textu.
+             Text se týká oftalmologie a obsahuje odborné výrazy. Ujisti se, že obzvláště tyto léky a oční kapky jsou správně napsány: 
+             Pokus se z kontextu odhadnout, co by mohlo být správné. Zde je seznam očních kapek, které se v textu často vyskytují: Tobradex, Maxitrol.
+             Pokud v textu bude zmínka o doporučeních chci aby jsi vrátil v jsonu také klíč "recommendations" a hodnotou s polem jednotlivých doporučení.
+             Vracej json ve formátu {"text": "opravený text", "recommendations": [recommendation1, recommendation2, ...], pokud v textu nebude žádná zmínka
+             o doporučeních, vracej pro klíč recommendations prázdné pole.}
+            V případě, že si nejsi jistý, můžeš nechat text nezměněný. Odpovídej POUZE V ČEŠTINĚ."""},
+            {"role": "user", "content": "Zde je zmíněný text: " + text}
+        ]
+    )
+    response = completion.choices[0].message.content
+    response = json.loads(response)
+    if response.get("recommendations") == []:
+        response_text = response.get("text")
+    else:
+        response_text = response.get(
+            "text") + "\nDoporučuji:\n" + "\n".join([f"• {rec}" for rec in response.get("recommendations", [])])
+    return response_text
 
 
 if __name__ == '__main__':
