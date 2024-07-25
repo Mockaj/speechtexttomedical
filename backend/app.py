9eb9251975c3b009295bd8a5151f612b674312fc
Ladislav Husty <lada.husty@gmail.com>
2024-07-25 20:36:09 +0200

    reformat and add langsmith tracing

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 import os
 import json
+import openai
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from openai import OpenAI
+from langsmith import traceable
 from medications import medications
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
@@ -52,25 +54,24 @@ def transcribe_audio():
                 prompt=medications
             )
 
-            postprocessed_text = postprocess_text(transcription)
+            postprocessed_text = run_pipeline(transcription)
             return jsonify({"transcription": postprocessed_text}), 200
     except Exception as e:
         print(str(e))
         return jsonify({"error": str(e)}), 500
 
 
-def postprocess_text(text: str) -> dict:
-    completion = client.chat.completions.create(
-        model="gpt-4o",  # gpt-3.5-turbo, gpt-4-turbo-preview
-        response_format={"type": "json_object"},
-        temperature=0.3,
-        messages=[
-            {"role": "system", "content": """
+@traceable
+def format_prompt(text: str) -> list:
+    return [
+        {
+            "role": "system",
+            "content": """
 __ASK__
 You are a professional copywriter tasked with correcting misspellings and grammatical errors in a text containing an ophthalmologist's diagnosis. The text may include technical terms and recommendations. Your goal is to produce a corrected version of the text and extract any recommendations mentioned. 
 
 __CONTEXT__
-The text is in Czech language and is produced by speech to text conversion. The conversion is not entirely precise and thus there are some mistakes. It is your task to correct these mistakes. It is important to consider the fact that it is an ophtalmologist diagnosis that is included in the provided text so correct this mistakes in such a way that the diagnosis makes sense.
+The text is in Czech language and is produced by speech to text conversion. The conversion is not entirely precise and thus there are some mistakes. It is your task to correct these mistakes. It is important to consider the fact that it is an ophthalmologist diagnosis that is included in the provided text so correct these mistakes in such a way that the diagnosis makes sense.
 
 __CONSTRAINTS__
 Follow these steps to complete the task:
@@ -84,7 +85,6 @@ Follow these steps to complete the task:
 4. As you review the text, look for any mentions of recommendations. These might be introduced with phrases like "I recommend," "it is advised," or "the patient should."
 
 5. If you find any recommendations, extract them and prepare to list them separately.
-
 
 6. If there are no recommendations mentioned in the text, include an empty array for the "recommendations" key, like this:
 
@@ -105,18 +105,38 @@ After correcting the text and identifying any recommendations, prepare your outp
   "text": "Your corrected version of the text goes here, excluding any recommendations",
   "recommendations": ["Recommendation 1", "Recommendation 2", "etc."]
 }
-"""},
-            {"role": "user", "content": "Prosím oprav podle instrukcí tento text: " + text}
-        ]
+"""
+        },
+        {
+            "role": "user",
+            "content": f"Prosím oprav podle instrukcí tento text: {text}"
+        }
+    ]
+
+@traceable(run_type="llm")
+def invoke_llm(messages: list) -> dict:
+    return openai.chat.completions.create(
+        messages=messages, model="gpt-4o", temperature=0.7, response_format={"type": "json_object"}
     )
-    response = completion.choices[0].message.content
-    response = json.loads(response)
-    if response.get("recommendations") == []:
-        response_text = response.get("text")
+
+@traceable
+def parse_output(response: dict) -> str:
+    response_content = response.choices[0].message.content
+    response_json = json.loads(response_content)
+    
+    if response_json.get("recommendations") == []:
+        response_text = response_json.get("text")
     else:
-        response_text = response.get(
-            "text") + "\nDoporučuji:\n" + "\n".join([f"• {rec}" for rec in response.get("recommendations")])
+        recommendations = "\nDoporučení:\n" + "\n".join([f"• {rec}" for rec in response_json.get("recommendations")])
+        response_text = response_json.get("text") + recommendations
+    
     return response_text
+
+@traceable
+def run_pipeline(text: str) -> str:
+    messages = format_prompt(text)
+    response = invoke_llm(messages)
+    return parse_output(response)
 
 
 if __name__ == '__main__':
